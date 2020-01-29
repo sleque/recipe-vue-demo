70aa948bbd2cc73e97828f335db9d29f5ed28bc8
Sleque <info@sleque.com>
2020-01-28 19:43:39 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Recipe />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import Recipe from './components/Recipe.vue'
+
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Recipe
   }
 }
 </script>
---
+++ src/components/Recipe.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- template is html  -->
+    <div class="recipe-component">
+        <p>Goodbye World.</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'recipe',
+  props: {
+    msg: String
+  }    
+}
+</script>
+
+<style scoped>
+
+</style>
